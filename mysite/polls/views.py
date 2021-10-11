3ceb53e01d9e0ca140743ca6b017b01eebe0c18a
Ajay-Nair <ajaynair000@gmail.com>
2021-10-11 23:42:52 +0530

    corrected views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,7 +11,7 @@ def detail(request , question_id):
     return HttpResponse("You are looking at question %s."%question_id)
 
 def results(request , question_id):
-    response =  HttpResponse("The results of question %s")
+    response = ("The results of question %s")
     return(response % question_id)
 
 def vote(request , question_id):
