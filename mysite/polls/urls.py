1bc1a6fff84b79abdc9883ffd3e035532b9c5853
Ajay-Nair <ajaynair000@gmail.com>
2021-10-11 23:29:28 +0530

    added path of urls to view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/',views.details,name = 'details'),
+    path('int:question_id/results/',views.detail,name='results'),
+    path('int:question_id/vote/',views.detail,name='vote'),
+
 ]
